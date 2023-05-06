e0f87798ca47b3269a9114f0a05f7927fc741541
Vikas preet Tiwana <vikastiwana8@gmail.com>
2023-05-06 16:58:09 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  addingEventListener();
+});
+
+function addingEventListener() {
+  const text = document.getElementById("text");
+  text.textContent = "This is really cool!";
+}
